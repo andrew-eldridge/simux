f8dfc0506073dcba3cd4ac20aae0bdc1b81c69f8
Andrew Eldridge <eldridga@email.sc.edu>
2024-04-23 06:08:47 -0400

    added new test case, command line options

--- testcase.py
+++ testcase.py
@@ -5,12 +5,17 @@ Full test case for a simple simulation environment
 """
 
 import logging
+import argparse
 from generators import exp_agg_generator, exp_generator, tria_generator
 from modules import *
 from environment import Environment
 
 
 def testcase_1() -> Tuple[Environment, List[Resource]]:
+    """
+    Minimal test case; simple Create and Dispose simulation
+    """
+
     server = Resource(
         name='Server',
         capacity=3
@@ -34,6 +39,10 @@ def testcase_1() -> Tuple[Environment, List[Resource]]:
 
 
 def testcase_2() -> Tuple[Environment, List[Resource]]:
+    """
+    Test case for a single server (M/M/1) queue with Seize/Delay/Release modules
+    """
+
     server = Resource(
         name='Server',
         capacity=3
@@ -71,14 +80,10 @@ def testcase_2() -> Tuple[Environment, List[Resource]]:
 
 
 def testcase_3() -> Tuple[Environment, List[Resource]]:
-    server1 = Resource(
-        name='Server 1',
-        capacity=3
-    )
-    server2 = Resource(
-        name='Server 2',
-        capacity=2
-    )
+    """
+    Test case for a Duplicate and Batch process using Delay and variable/attribute Assigns.
+    Entities are assigned a unique identifier, duplicated, delayed, and batched by their unique identifier.
+    """
 
     def assign_last_entity_handler(variables: dict, entity_attr: dict) -> any:
         return variables['last_entity'] + 1
@@ -132,17 +137,135 @@ def testcase_3() -> Tuple[Environment, List[Resource]]:
         module_chains=[mod_chain],
         variables=[('last_entity', 0)]
     )
-    resources = [server1, server2]
 
-    return env, resources
+    return env, []
+
+
+def testcase_4() -> Tuple[Environment, List[Resource]]:
+    """
+    Extension of Test Case 4; simulates a Duplicate and Batch process using multiple Servers shared across Seize/Delay/Release groups.
+    Entities are assigned a unique identifier, duplicated, seize one of several resources, delay, release the resource,
+    are batched by their unique identifier, seize a resource accessed in a previous step, delay, release the resource, and dispose.
+    """
+
+    def assign_last_entity_handler(variables: dict, entity_attr: dict) -> any:
+        return variables['last_entity'] + 1
+
+    def assign_entity_id_handler(variables: dict, entity_attr: dict) -> any:
+        return variables['last_entity']
+
+    server1 = Resource(
+        name='Server 1',
+        capacity=3
+    )
+    server2 = Resource(
+        name='Server 2',
+        capacity=2
+    )
+
+    batch_mod_chain = BatchModule(
+        name='Batch 1',
+        batch_type=BatchType.ATTRIBUTE,
+        batch_size=2,
+        batch_attr='entity_id',
+        batch_entity_type='Person Group',
+        next_module=SeizeModule(
+            name='Seize Server 1 Again',
+            resource=server1,
+            num_resources=1,
+            next_module=DelayModule(
+                name='Delay 3',
+                delay_generator=tria_generator(0, 3),
+                cost_allocation=CostType.NON_VALUE_ADDED,
+                next_module=ReleaseModule(
+                    name='Release Server 1 Again',
+                    resource=server1,
+                    num_resources=1,
+                    next_module=DisposeModule(
+                        name='Dispose 1'
+                    )
+                )
+            )
+        )
+    )
+
+    mod_chain = CreateModule(
+        name='Create Person',
+        gen_entity_type='Person',
+        arrival_generator=exp_agg_generator(1),
+        next_module=AssignModule(
+            name='Assign entity ID',
+            assignments=[
+                Assignment(assign_type=AssignType.VARIABLE, assign_name='last_entity', assign_value_handler=assign_last_entity_handler),
+                Assignment(assign_type=AssignType.ATTRIBUTE, assign_name='entity_id', assign_value_handler=assign_entity_id_handler)
+            ],
+            next_module=DuplicateModule(
+                name='Duplicate 1',
+                next_module_orig=SeizeModule(
+                    name='Seize Server 1',
+                    resource=server1,
+                    num_resources=1,
+                    next_module=DelayModule(
+                        name='Delay 1',
+                        delay_generator=exp_generator(0.5),
+                        next_module=ReleaseModule(
+                            name='Release Server 1',
+                            resource=server1,
+                            num_resources=1,
+                            next_module=batch_mod_chain
+                        )
+                    )
+                ),
+                next_module_dup=SeizeModule(
+                    name='Seize Server 2',
+                    resource=server2,
+                    num_resources=1,
+                    next_module=DelayModule(
+                        name='Delay 1',
+                        delay_generator=tria_generator(0, 2),
+                        next_module=ReleaseModule(
+                            name='Release Server 2',
+                            resource=server2,
+                            num_resources=1,
+                            next_module=batch_mod_chain
+                        )
+                    )
+                )
+            )
+        )
+    )
+
+    env = Environment(
+        module_chains=[mod_chain],
+        variables=[('last_entity', 0)]
+    )
+
+    return env, []
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--case', type=int, default=4, help='Test case number (1, 2, 3, 4).')
+    parser.add_argument('-d', '--debug', action='store_true', help='Flag for debugging outputs.')
+    parser.add_argument('-D', '--duration', type=float, default=100., help='Duration of simulation in default time units.')
+    args = parser.parse_args()
 
-    env, resources = testcase_3()
-    sim_duration = 100
-    sys_var, sys_entity_metrics_df = env.run_simulation(sim_duration)
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+
+    match args.case:
+        case 1:
+            env, resources = testcase_1()
+        case 2:
+            env, resources = testcase_2()
+        case 3:
+            env, resources = testcase_3()
+        case 4:
+            env, resources = testcase_4()
+        case _:
+            raise ValueError('Invalid test case number provided:', args.case)
+
+    sys_var, sys_entity_metrics_df = env.run_simulation(args.duration)
 
     # system metric outputs
     print(sys_entity_metrics_df)
@@ -150,7 +273,7 @@ def main():
 
     # resource metric outputs
     for r in resources:
-        resource_utilization = r.calc_utilization(sim_duration)
+        resource_utilization = r.calc_utilization(args.duration)
         print(f'{r.name} resource utilization:', resource_utilization)
 
 
